2bea5dba393ca580d738d208ed35e6b6abe5e80b
the4nna <the4nna@outlook.com>
2022-07-20 19:53:02 -0300

    Adicionando service e @Injectable

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { Course } from "./course";
+import { CourseService } from './course.service';
 
 @Component({
     selector: 'app-course-list',
@@ -11,28 +12,11 @@ export class CourseListComponent implements OnInit {
     title: string = "Course List"
     courses: Course[] = [];
 
+    constructor(private courseService: CourseService) {
+
+    }
+
     ngOnInit(): void {
-        this.courses = [
-            {
-                id: 1,
-                name: 'Angular: Forms',
-                imageUrl: './assets/images/forms.png',
-                price: 99.99,
-                code: 'XPS-8796',
-                duration: 120,
-                rating: 4.5,
-                releaseDate: 'May, 27 - 2022'
-            },
-            {
-                id: 2,
-                name: 'Angular: HTTP',
-                imageUrl: './assets/images/http.png',
-                price: 45.99,
-                code: 'LKL-1094',
-                duration: 80,
-                rating: 4,
-                releaseDate: 'July, 19 - 2022'
-            }
-        ]
+        this.courses = this.courseService.retrieveAll()
     }
 }
